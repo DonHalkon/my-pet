7fb4dc7f60a60d086c8559268dd853c653a82998
Sviatoslav Naiden <sv.nayden@gmail.com>
2019-12-14 20:01:34 +0100

    init

---
+++ src/main/java/me/naiden/mypet/MyPetApplication.java
@@ -0,0 +1,13 @@
+package me.naiden.mypet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPetApplication.class, args);
+	}
+
+}
