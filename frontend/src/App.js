aa6fba20c0873e9596d2a3931f7eda73d1b95910
Sviatoslav Naiden <sv.nayden@gmail.com>
2020-02-29 00:05:58 +0100

    basic react implementation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,66 @@
-import React from 'react';
-import './css/App.css';
-import PetProjectApp from './components/PetProjectApp'
+import React, { Component } from 'react';
+import { Button, Nav, Navbar} from "react-bootstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <PetProjectApp />
-    </div>
-  );
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+
+import PetProjectApp from './components/PetProjectApp'
+import PetProjectJumbotron from './components/PetProjectJumbotron'
+import PetProjectDataService from './service/PetProjectDataService'
+import PetProjectCardDeck from './components/PetProjectCardDeck';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      petProjects: [],
+      message: null
+  }
+    this.refreshPetProjects = this.refreshPetProjects.bind(this)
+  }
+
+  componentDidMount() {
+    this.refreshPetProjects();
+  }
+
+  refreshPetProjects() {
+    PetProjectDataService.retrieveAllPetProjects()
+      .then(response => {
+        
+        this.setState({ petProjects: response.data })
+      });
+  }
+
+  detailedView(petProjectName) {
+    PetProjectDataService.retrievePetProjectByName(petProjectName)
+    .then(response => {
+      console.log(response);
+    }) 
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="/">PET PROJECT</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+          </Nav>
+          <Button variant="outline-info">Log in</Button>
+        </Navbar>
+
+        <PetProjectJumbotron />
+        <PetProjectApp />
+        <PetProjectCardDeck 
+          petProjects={this.state.petProjects}
+          detailedView={this.detailedView}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
