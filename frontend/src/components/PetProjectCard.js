c3d22d0a6fe38876ae27b048fa51d91ce22abc60
Sviatoslav Naiden <sv.nayden@gmail.com>
2020-02-29 22:47:41 +0100

    frontend: more beautiful views

--- frontend/src/components/PetProjectCard.js
+++ frontend/src/components/PetProjectCard.js
@@ -2,27 +2,27 @@ import React, { Component } from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import { Link } from "react-router-dom";
-import PetProjectPage from '../Pages/PetProjectPage';
+import { MdPets } from 'react-icons/md';
+import defaultImg from '../img/default.jpg'
 
 class PetProjectCard extends Component {
 
     render() {
         return (
             <Card>
-                <Card.Img variant="top" src="holder.js/50px80" />
-                <Card.Body>
-                    <Card.Title><Link to={'/pet-projects/' + this.props.petProject.name}>{this.props.petProject.name}</Link></Card.Title>
-                    <Card.Title></Card.Title>
-                    <Card.Text>
-                        {this.props.petProject.shortDescription}
-                    </Card.Text>
-                </Card.Body>
-                <Card.Footer>
-                    <small className="text-muted">{this.props.petProject.task}</small>
-                </Card.Footer>
-                <Button variant="outline-primary" type="button"
-                    onClick={() => this.props.detailedView(this.props.petProject.name)}>Details</Button>
-                
+                <Card.Img variant="top" src={defaultImg} />
+                <Card.ImgOverlay>
+                    <Card.Body>
+                        <Card.Title><MdPets /> {this.props.petProject.name}</Card.Title>
+                        <Card.Subtitle className="mb-2">
+                            {this.props.petProject.shortDescription}
+                        </Card.Subtitle>
+                        <Card.Text>
+                            Task: {this.props.petProject.task}
+                        </Card.Text>
+                        <Card.Link href={`/pet-projects/${this.props.petProject.name}`}>Details</Card.Link>
+                    </Card.Body>
+                </Card.ImgOverlay>
             </Card>
         )
     }
--- frontend/src/components/PetProjectJumbotron.js
+++ frontend/src/components/PetProjectJumbotron.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Button from 'react-bootstrap/Button';
+import { Container } from 'react-bootstrap';
 
 class PetProjectJumbotron extends Component {
     render() {
         return (
-            <Jumbotron>
-                <h1>MY PET PROJECT</h1>
-                <p>... is a pet project about pet projects</p>
-                <p>
-                    <Button variant="primary">Learn more</Button>
-                </p>
+            <Jumbotron fluid>
+                <Container>
+                    <h1>MY PET PROJECT</h1>
+                    <p>... is a pet project about pet projects</p>
+                </Container>
             </Jumbotron>
         )
     }
